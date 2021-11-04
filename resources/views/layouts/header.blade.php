7b2d6b7d40b0aae11e4d4c2fb5c02d668d1fbe8a
elgun837 <elgun.xalilov@bk.ru>
2021-11-04 15:18:21 +0400

    create-laravel-app running

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,9 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Laravel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <img class="logo-image" src="https://laravel.com/img/logotype.min.svg">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
