ed7afc745054e9f8f0592875fd635c815c4fa81c
elgun837 <elgun.xalilov@bk.ru>
2021-11-09 15:37:46 +0400

    runtime

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,9 @@
 
 
 @section('content')
-
+<div class="wrapper">
+   <video autoplay loop muted class="wrapper__video">
+      <source src="//vimeo-hp-videos.global.ssl.fastly.net/3/3.mp4">
+   </video>  
+</div>
 @endsection
